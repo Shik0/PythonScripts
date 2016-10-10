6de174e80b96874152a5ca2f2f0e79c60fa93f8f
shahin.mikailov <mikailovsh@gmail.com>
2016-10-10 15:22:01 +0400

    Greatest common divisor

---
+++ Greatest_common_divisor.py
@@ -0,0 +1,20 @@
+def gcdIter(a, b):
+    '''
+    a, b: positive integers
+
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    result = 0
+    maxx = max(a,b)
+    return max([x for x in range(maxx,0,-1) if a%x==0 and b%x==0])
+    '''
+    #or by other way
+    while maxx > 0 :
+        if a % maxx == 0 and b % maxx == 0:
+            result = maxx
+            break
+        maxx -= 1
+    return result
+    '''
+print(gcdIter(6,12))
